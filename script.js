901a186fcb733ca1e7333ed3bb5cd218a38d7358
yididya kebede <96166723+jedinna@users.noreply.github.com>
2024-03-01 00:39:38 +0300

    making the navigation stickey

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-const btnNav =document.querySelector(".btn-mobile.nav");
-const headerEl= document.querySelector(".header");
+console.log("fuckyou");
+const btnNav = document.querySelector(".btn-mobile-nav ");
+const headerEl = document.querySelector(".header");
 
-btnNav.addEventListener('click',function(){
-    headerEl.classList.toggle("nav-open");
-})
+btnNav.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+const sectionheroel = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(function (entries) {
+    const ent = entries[0];
+    if(ent.isIntersecting ===false){
+        document.body.classList.add('stickey')
+    } if(ent.isIntersecting ){
+        document.body.classList.remove('stickey')
+    }
+   
+   
+    
+}, {
+  root: null,
+  threshold: 0,
+  rootMargin:'-80px',
+});
+
+obs.observe(sectionheroel);
